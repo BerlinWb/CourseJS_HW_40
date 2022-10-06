63880d676a1512a9ee62eca13848fd087e28eb14
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-10-06 13:18:41 +0300

    Add files via upload

---
+++ CourseJS_HW_40/js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(() => {
+    document
+        .querySelector('#form')
+        .addEventListener(
+            'submit',
+            (evt) => {
+                evt.preventDefault()
+                const {target: form} = evt
+
+                const inputs = form.querySelectorAll('input, select, textarea')
+
+                const formData = {}
+                for (const input of inputs) {
+                    const {name, value, checked} = input
+                    if (checked) {
+                        formData[name] = checked
+                    } else {
+                        formData[name] = value
+                    }
+                }
+                localStorage.setItem('formData', JSON.stringify(formData))
+                setTimeout(() => {
+                    window.location.href = 'solutions.html'
+                }, 2000);
+            })
+
+
+})();
